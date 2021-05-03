5bf24b94d6553450702d1232a8c632c246bd7a55
Ericgacoki <gacokieric@gmail.com>
2021-05-04 00:43:10 +0300

    Implemented viewBinding and shimmer logic

--- app/src/main/java/com/ericg/shimmerloading/MainActivity.kt
+++ app/src/main/java/com/ericg/shimmerloading/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.ericg.shimmerloading
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import androidx.appcompat.app.AppCompatActivity
+import com.ericg.shimmerloading.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        startShimmerLoading()
+        binding.btnRefresh.setOnClickListener {
+            startShimmerLoading()
+        }
     }
+
+    private fun startShimmerLoading() {
+        binding.mainLayout.visibility = GONE
+        binding.shimmerLayout.apply {
+            visibility = VISIBLE
+            startShimmer()
+        }
+
+        CoroutineScope(Main).launch(Main) {
+            delay(2500)
+            stopShimmerLoading()
+        }
+    }
+
+    // Normally this should happen after a network request 😎
+    private fun stopShimmerLoading() {
+        binding.shimmerLayout.apply {
+            clearAnimation()
+            stopShimmer()
+            visibility = GONE
+        }
+        binding.mainLayout.visibility = VISIBLE
+    }
+
 }
